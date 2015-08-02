036405d5b49e09ed5292ca7ebc6a8c7178da1b39
keik <k4t0.kei@gmail.com>
2015-08-03 08:27:31 +0900

    Add eslint for js2-mode

--- init.el
+++ init.el
@@ -413,6 +413,7 @@
 
 ;; flycheck
 (add-hook 'html-mode-hook 'flycheck-mode)
+(add-hook 'js2-mode-hook 'flycheck-mode)
 (add-hook 'python-mode-hook 'flycheck-mode)
 (add-hook 'java-mode-hook 'flycheck-mode)
 
