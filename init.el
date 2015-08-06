1c1157aaa9a2e5904409b27b4de8e658e01f79fa
keik <k4t0.kei@gmail.com>
2015-08-07 00:37:10 +0900

    Add js2-mode for *.es files

--- init.el
+++ init.el
@@ -460,6 +460,7 @@
 ;; js2-mode
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.es$" . js2-mode))
 
 ;; jade-mode
 (when (require 'sws-mode nil 'noerror))
