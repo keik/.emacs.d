d363ef33c3a1b0002c615a622c2f07a7f9b79fc6
keik <k4t0.kei@gmail.com>
2015-09-03 10:38:03 +0900

    Fix ext pattern for auto-mode-alist

--- inits/20-prg-html.el
+++ inits/20-prg-html.el
@@ -1,8 +1,8 @@
 (when (require 'web-mode nil 'noerror)
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.jsp?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.ect?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tag?\\'" . web-mode)) ;; for Riot.js tag
+  (add-to-list 'auto-mode-alist '("\\.html?$" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.jsp$" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.ect$" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tag$" . web-mode)) ;; for Riot.js tag
   (define-key web-mode-map (kbd "C-c C-v") 'browse-url-of-buffer))
 
 (setq browse-url-browser-function 'browse-url-generic
