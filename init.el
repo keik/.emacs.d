0772e5ff4b25af7f4c8034281220fe0ffe59b07b
keik <k4t0.kei@gmail.com>
2015-12-07 09:45:54 +0900

    Update

--- init.el
+++ init.el
@@ -33,6 +33,12 @@
        ;;; Java
        ;; malabar-mode
 
+       ;;; YAML
+       yaml-mode
+
+       ;;; PHP
+       php-mode
+
        ;;; HTML / CSS/ JavaScript
        ;; sws-mode jade-mode
        web-mode
@@ -69,3 +75,17 @@
 ;; load inits
 (require 'init-loader)
 (init-loader-load)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/todo.org")))
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background "black"))))
+ '(term-color-blue ((t (:background "deep sky blue" :foreground "deep sky blue")))))
--- inits/10-global-keybinds.el
+++ inits/10-global-keybinds.el
@@ -3,3 +3,5 @@
   (if (and (called-interactively-p) (not (region-active-p)))
     (backward-kill-word 1)
     ad-do-it))
+
+(global-set-key (kbd "C-c a") 'org-agenda)
