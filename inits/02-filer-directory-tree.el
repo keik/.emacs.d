eb7391707212495adddfa50d167a5dcc79e4acdb
keik <k4t0.kei@gmail.com>
2016-07-11 18:16:55 +0900

    Change filer from neotree to speedbar

--- inits/02-filer-directory-tree.el
+++ inits/02-filer-directory-tree.el
@@ -1,3 +1,8 @@
-(unless (require 'neotree nil t)
-  (package-install 'neotree))
-(global-set-key (kbd "C-x C-j") 'neotree-toggle)
+(unless (require 'sr-speedbar nil t)
+  (package-install 'sr-speedbar))
+(global-set-key (kbd "C-x C-j") 'sr-speedbar-toggle)
+
+
+;;(unless (require 'neotree nil t)
+;;  (package-install 'neotree))
+;;(global-set-key (kbd "C-x C-j") 'neotree-toggle)
