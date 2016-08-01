f502b9958165aa83b477a06ce2b0b455e9c27a11
keik <k4t0.kei@gmail.com>
2016-08-02 00:41:27 +0900

    Update config for speedbar to show all files

--- inits/02-filer-directory-tree.el
+++ inits/02-filer-directory-tree.el
@@ -1,8 +1,4 @@
 (unless (require 'sr-speedbar nil t)
   (package-install 'sr-speedbar))
 (global-set-key (kbd "C-x C-j") 'sr-speedbar-toggle)
-
-
-;;(unless (require 'neotree nil t)
-;;  (package-install 'neotree))
-;;(global-set-key (kbd "C-x C-j") 'neotree-toggle)
+(setq speedbar-show-unknown-files t)
