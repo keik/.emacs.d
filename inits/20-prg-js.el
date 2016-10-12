489b9166c0c80768bcdc39cc4333163ddf71a1df
keik <k4t0.kei@gmail.com>
2016-10-12 23:46:39 +0900

    Fix indent value

--- inits/20-prg-js.el
+++ inits/20-prg-js.el
@@ -6,7 +6,7 @@
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-attr-indent-offset nil)
+  (setq web-mode-attr-indent-offset 2)
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
