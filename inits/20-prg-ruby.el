77d4566f4c55af6012b4ce80217b1d878d0a54e3
keik <keik@c-fo.com>
2017-03-03 09:20:56 +0900

    Bye robe-mode

--- inits/20-prg-ruby.el
+++ inits/20-prg-ruby.el
@@ -1,8 +1,1 @@
-(unless (require 'robe nil t)
-  (package-install 'robe))
-
-(add-hook 'ruby-mode-hook 'robe-mode)
-(add-hook 'robe-mode-hook
-  '(lambda ()
-     (ac-robe-setup)
-     (add-to-list 'ac-source-robe '(requires . -1))))
+(setq ruby-insert-encoding-magic-comment nil)
