c32a02352eecd3f6150faf27f9590213a7dc4357
keik <keik@c-fo.com>
2017-03-03 09:20:03 +0900

    Fix arguments error

--- inits/12-keys-C-w-delete-backward.el
+++ inits/12-keys-C-w-delete-backward.el
@@ -1,5 +1,5 @@
 ;; when region not active, C-w do `backward-kill-word` instead of `kill-region`
 (defadvice kill-region (around kill-word-or-kill-region activate)
-  (if (and (called-interactively-p) (not (region-active-p)))
+  (if (and (called-interactively-p "interactive") (not (region-active-p)))
     (backward-kill-word 1)
     ad-do-it))
