b89c6b9b4282ec6822074e593db636b6cfcde378
keik <k4t0.kei@gmail.com>
2017-12-01 00:02:19 +0900

    Load snippets dir

--- inits/20-prg-snippets.el
+++ inits/20-prg-snippets.el
@@ -1,3 +1,5 @@
 (unless (require 'yasnippet nil 'noerror)
   (package-install 'yasnippet))
 (yas-global-mode 1)
+(with-eval-after-load 'yasnippet
+  (setq yas-snippet-dirs (remq 'yas-installed-snippets-dir yas-snippet-dirs)))
