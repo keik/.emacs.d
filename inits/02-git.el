ee180456b29c48a3c6dba762c271f722d8e03948
keik <k4t0.kei@gmail.com>
2017-12-16 22:59:34 +0900

    Move configs

--- inits/02-git.el
+++ inits/02-git.el
@@ -2,3 +2,9 @@
   (package-install 'magit))
 
 (setq magit-blame-heading-format "%.7H %-20a %C %s")
+
+;; show chagnes on Git at line
+(unless (require 'git-gutter nil t)
+  (package-install 'git-gutter))
+(global-git-gutter-mode +1)
+(git-gutter:linum-setup)
--- inits/20-prg-common.el
+++ inits/20-prg-common.el
@@ -1,8 +1,3 @@
 (unless (require 'editorconfig nil t)
   (package-install 'editorconfig))
 (editorconfig-mode)
-
-(unless (require 'git-gutter nil t)
-  (package-install 'git-gutter))
-(global-git-gutter-mode +1)
-(git-gutter:linum-setup)
