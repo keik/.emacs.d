d26659b92efacdd0724afe4499a1d8e0fe56d61c
keik <k4t0.kei@gmail.com>
2017-12-16 22:55:09 +0900

    Add keys for helm

--- inits/00-env-helm.el
+++ inits/00-env-helm.el
@@ -3,3 +3,10 @@
 
 (require 'helm-config)
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x C-r") 'helm-recentf)
+(setq recentf-max-saved-items 2000)
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-z")  'helm-select-action)
+(define-key helm-map (kbd "C-w")  'kill-region)
