46ebe25311f01a607f6d0455a634957ae5f0b1b7
keik <keik@c-fo.com>
2018-02-04 23:06:19 +0900

    Disable invalid configurations

--- inits/cocoa-emacs-im.el
+++ inits/cocoa-emacs-im.el
@@ -1,4 +1,4 @@
-(setq default-input-method "MacOSX")
-(mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" 'title "あ")
-(mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" 'cursor-color "#3a3")
-(mac-set-input-method-parameter "com.google.inputmethod.Japanese.Roman" 'cursor-color "#666")
+;; (setq default-input-method "MacOSX")
+;; (mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" 'title "あ")
+;; (mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" 'cursor-color "#3a3")
+;; (mac-set-input-method-parameter "com.google.inputmethod.Japanese.Roman" 'cursor-color "#666")
