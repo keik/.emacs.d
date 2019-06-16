912c4ab8a577060e75e80e12d2b12cd2f5c7c349
keik <keik@c-fo.com>
2019-06-17 00:52:34 +0900

    :+1: Setup tide-mode.

--- inits/20-prg-js.el
+++ inits/20-prg-js.el
@@ -1,8 +1,6 @@
-(unless (require 'web-mode nil t)
-  (package-install 'web-mode))
+(use-package web-mode)
 
 (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.es$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx?$" . web-mode))
 
 (defun my-web-mode-hook ()
@@ -21,37 +19,43 @@
   (add-to-list 'web-mode-indentation-params '("lineup-quotes" . nil))
   (add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
   (add-to-list 'web-mode-indentation-params '("case-extra-offset" . nil))
-  (lambda ()
-    (when (string-equal "tsx" (file-name-extension buffer-file-name))
-      (setup-tide-mode)))
+  (when (string-equal "tsx" (file-name-extension buffer-file-name))
+    (setup-tide-mode))
   )
 
 (add-hook 'web-mode-hook 'my-web-mode-hook)
 
-(setq web-mode-content-types-alist
-  (if (boundp 'web-mode-content-types-alist) web-mode-content-types-alist '()))
-(add-to-list 'web-mode-content-types-alist '("jsx" . "\\.jsx?$"))
+;;; Forgotten the purpose of follows... remove this
+;; (setq web-mode-content-types-alist
+;;   (if (boundp 'web-mode-content-types-alist) web-mode-content-types-alist '()))
+;; (add-to-list 'web-mode-content-types-alist '("jsx" . "\\.jsx?$"))
+;;
+;; (defadvice web-mode-highlight-part (around tweak-jsx activate)
+;;   (message (format "web-mode-content-type: %s" web-mode-content-type))
+;;   (if (equal web-mode-content-type "jsx")
+;;     (let ((web-mode-enable-part-face nil))
+;;       ad-do-it)
+;;      ad-do-it))
 
-(defadvice web-mode-highlight-part (around tweak-jsx activate)
-  (message (format "web-mode-content-type: %s" web-mode-content-type))
-  (if (equal web-mode-content-type "jsx")
-    (let ((web-mode-enable-part-face nil))
-      ad-do-it)
-     ad-do-it))
+(use-package tide
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save))
+  :config
+  (setq company-tooltip-align-annotations t)
+  ;; Now no settings of TypeScript (Not TSX). Consider later.
+  ;; (add-hook 'typescript-mode-hook #'setup-tide-mode)
+  )
 
 (defun setup-tide-mode ()
+  "See https://github.com/ananthakumaran/tide ."
   (interactive)
   (tide-setup)
-  (flycheck-mode +1)
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1)
-  ;; company is an optional dependency. You have to
-  ;; install it separately via package-install
-  ;; `M-x package-install [ret] company`
   (company-mode +1))
 
-;; aligns annotation to the right hand side
-(setq company-tooltip-align-annotations t)
 
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
