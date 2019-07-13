25fb4042c165e60de66432b3d2f4998cc31c5613
keik <keik@c-fo.com>
2019-07-13 21:15:53 +0900

    :+1: Use company-mode formally.

--- inits/11-input-autocomplete.el
+++ inits/11-input-autocomplete.el
@@ -1,41 +1,3 @@
-;; Use company-mode experimentaly.
-;; remove later.
-
-;; (unless (require 'auto-complete nil t)
-;;   (package-install 'auto-complete))
-
-;; (require 'auto-complete-config)
-;; (ac-config-default)
-;; (setq ac-use-fuzzy t)
-;; (global-auto-complete-mode t)
-;; (define-key ac-mode-map (kbd "<tab>") 'ac-start)
-;; (define-key ac-completing-map (kbd "<tab>") 'auto-complete)
-;; (define-key ac-completing-map (kbd "C-n") 'ac-next)
-;; (define-key ac-completing-map (kbd "C-p") 'ac-previous)
-;; (setq ac-auto-show-menu 0)
-
-;; ;; Add ac-sources
-;; (defun ac-web-mode-setup ()
-;;   (add-to-list 'ac-sources 'ac-source-yasnippet)
-;;   (add-to-list 'ac-sources 'ac-source-filename))
-;; (add-hook 'web-mode-hook 'ac-web-mode-setup)
-
-;; (defun ac-css-mode-setup ()
-;;   (add-to-list 'ac-sources 'add-ac-source-yasnippet)
-;;   (add-to-list 'ac-sources 'ac-source-filename))
-;; (add-hook 'css-mode-hook 'ac-css-mode-setup)
-
-;; (defun ac-coffee-mode-setup ()
-;;   (add-to-list 'ac-sources 'ac-source-filename))
-;; (add-hook 'coffee-mode-hook 'ac-coffee-mode-setup)
-
-;; ;; 日本語入力時ac無効化
-;; ;; http://d.hatena.ne.jp/IMAKADO/20090813/1250130343
-;; (defadvice ac-word-candidates (after remove-word-contain-japanese activate)
-;;   (let ((contain-japanese (lambda (s) (string-match (rx (category japanese)) s))))
-;;     (setq ad-return-value
-;;           (remove-if contain-japanese ad-return-value))))
-
 (unless (require 'company nil t)
   (package-install 'company))
 
@@ -55,3 +17,17 @@
 
 ;; TABで候補を設定
 (define-key company-active-map (kbd "C-i") 'company-complete-selection)
+
+;; https://emacs.stackexchange.com/questions/10431/get-company-to-show-suggestions-for-yasnippet-names
+;; Add yasnippet support for all company backends
+;; https://github.com/syl20bnr/spacemacs/pull/179
+(defvar company-mode/enable-yas t
+  "Enable yasnippet for all backends.")
+
+(defun company-mode/backend-with-yas (backend)
+  (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+      backend
+    (append (if (consp backend) backend (list backend))
+            '(:with company-yasnippet))))
+
+(setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
