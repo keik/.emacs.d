cafbc1c45edfc94dfbba520b70612a16e4e50fa6
KATO Kei <keik@c-fo.com>
2023-12-11 13:11:14 +0900

    fix: Set `helm-alow-mouse` to nil. see https://github.com/yasuyk/helm-git-grep/issues/52

--- inits/02-git.el
+++ inits/02-git.el
@@ -1,3 +1,6 @@
+;;; 02-git.el --- settings for Git
+;;; Commentary:
+;;; Code:
 (use-package magit
   :ensure t
   :config
@@ -8,9 +11,12 @@
   :ensure t
   :bind
   ("C-c g" . helm-git-grep)
+  :config
+  (setq helm-allow-mouse nil)
 )
 ;; show chagnes on Git at line
 ;; (unless (require 'git-gutter nil t)
 ;;   (package-install 'git-gutter))
 ;; (global-git-gutter-mode +1)
 ;; (git-gutter:linum-setup)
+;;; 02-git.el ends here
