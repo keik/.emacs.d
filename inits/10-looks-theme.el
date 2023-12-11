44dbb6e9b1567e8bb29fd037e472c86c4f35d32a
keik <keik@c-fo.com>
2023-12-11 13:03:36 +0900

    feat: Update theme.

--- inits/10-looks-theme.el
+++ inits/10-looks-theme.el
@@ -1,18 +1,9 @@
+;;; 10-looks-theme.el --- settings for theme
+;;; Commentary:
+;;; Code:
 (unless (require 'cyberpunk-theme nil t)
   (package-install 'cyberpunk-theme))
-(load-theme 'cyberpunk t)
-
-(set-face-attribute 'mode-line nil
-  :height 120
-  :foreground "#88CCFF"
-  :background "#555555"
-  :box '(:line-width 1 :color "#88CCFF"))
-(set-face-attribute 'mode-line-inactive nil
-  :height 120
-  :foreground "#777777"
-  :background "#555555"
-  :box '(:line-width 1 :color "#333333"))
-(set-face-attribute 'mode-line-buffer-id nil
-  :foreground "#DDDDDD")
-
-(set-frame-parameter nil 'alpha '(85 70))
+ (load-theme 'cyberpunk t)
+(set-face-background 'default "unspecified-bg")
+(set-face-foreground 'font-lock-comment-face "#CCCCCC")
+;;; 10-looks-theme.el ends here
