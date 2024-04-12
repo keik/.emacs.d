42057be67dd4b839d1971ffffe9bd790babd7939
KATO Kei <keik@c-fo.com>
2024-04-12 09:44:06 +0900

    chore: Remove outdated settings.

--- inits/20-prg-js.el
+++ inits/20-prg-js.el
@@ -5,7 +5,6 @@
    "\\.jsx$"
   :config
   (add-hook 'web-mode-hook #'(lambda () (setq web-mode-enable-auto-indentation nil)))
-  ;;(add-hook 'web-mode-hook 'my/web-mode-on-js)
   ;;(setq web-mode-enable-auto-indentation nil)
   ;;(setq web-mode-attr-indent-offset 2)
   ;;(setq web-mode-markup-indent-offset 2)
@@ -21,9 +20,3 @@
   ;;(add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
   ;;(add-to-list 'web-mode-indentation-params '("case-extra-offset" . nil))
   )
-
-(defun my/web-mode-on-js ()
-  "Enable tide-mode only .tsx file."
-  (when (string-equal "js" (file-name-extension buffer-file-name))
-    (flycheck-add-next-checker 'javascript-eslint 'lsp)
-    ))
