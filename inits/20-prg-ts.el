059f318452518e81013c535cf12fcdd0c18c3e51
KATO Kei <keik@c-fo.com>
2024-04-12 09:37:17 +0900

    feat: Support .mts.

--- inits/20-prg-ts.el
+++ inits/20-prg-ts.el
@@ -1,6 +1,11 @@
+;;; 20-prg-ts.el --- settings for TypeScript
+;;; Commentary:
+;;; Code:
 (use-package typescript-ts-mode
   :ensure t
   :mode
+  "\\.mts$"
   "\\.ts$"
   "\\.tsx$"
   )
+;;; 20-prg-ts.el ends here
