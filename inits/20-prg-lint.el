98a9f159fe977bbd1a20dcc6b051a3bbc2d365be
KATO Kei <keik@c-fo.com>
2024-05-02 11:25:28 +0900

    chore: Organize order.

--- inits/20-prg-lint.el
+++ inits/20-prg-lint.el
@@ -1,27 +1,6 @@
 ;;; 20-prg-lint.el --- settings for Lint
 ;;; Commentary:
 ;;; Code:
-(flycheck-define-checker bundle/ruby-rubocop
-  "A Ruby syntax and style checker using the RuboCop tool.
-   Puts .dir-locals.el to each project root with contents
-
-     ((ruby-mode . ((flycheck-checker . bundle/ruby-rubocop))))
-
-  "
-  :command
-  ("bundle" "ex" "rubocop" "--format" "emacs"
-   (config-file "--config" flycheck-rubocoprc)
-   source)
-  :error-patterns
-  ((warning line-start
-            (file-name) ":" line ":" column ": " (or "C" "W") ": " (message)
-            line-end)
-   (error line-start
-          (file-name) ":" line ":" column ": " (or "E" "F") ": " (message)
-          line-end))
-  :modes (ruby-mode)
-  )
-
 (use-package flycheck
   :ensure t
   :config
@@ -40,6 +19,26 @@
                   )
                 )
 
+  (flycheck-define-checker bundle/ruby-rubocop
+    "A Ruby syntax and style checker using the RuboCop tool.
+   Puts .dir-locals.el to each project root with contents
+
+     ((ruby-mode . ((flycheck-checker . bundle/ruby-rubocop))))
+
+  "
+    :command
+    ("bundle" "ex" "rubocop" "--format" "emacs"
+     (config-file "--config" flycheck-rubocoprc)
+     source)
+    :error-patterns
+    ((warning line-start
+              (file-name) ":" line ":" column ": " (or "C" "W") ": " (message)
+              line-end)
+     (error line-start
+            (file-name) ":" line ":" column ": " (or "E" "F") ": " (message)
+            line-end))
+    :modes (ruby-mode)
+    )
   (add-to-list 'flycheck-checkers 'bundle/ruby-rubocop t)
 
   :hook
