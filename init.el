90df8083e5f1f871102560b6ba399d3cb319313f
KATO Kei <keik@c-fo.com>
2024-10-08 22:21:11 +0900

    refactor.

--- init.el
+++ init.el
@@ -1,14 +1,8 @@
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
+
 (require 'package)
-(setq package-check-signature nil)
-(add-to-list 'package-archives '("elpa" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-
-(package-initialize)
-;;(package-refresh-contents)
-
-(unless (require 'use-package nil t)
-  (package-install 'use-package))
 
 ;; load inits
 (unless (require 'init-loader nil t)
