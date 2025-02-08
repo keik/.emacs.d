961052f476399a44ac0b43689b6f403033a628e5
KATO Kei <keik@c-fo.com>
2025-02-08 20:50:09 +0900

    :recycle: Quit init-loader.

--- init.el
+++ init.el
@@ -8,12 +8,6 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
-;; load inits
-;; TODO: Remove
-(unless (require 'init-loader nil t)
-  (package-install 'init-loader))
-(init-loader-load)
-
 ;;; ================================================================
 ;;; basic
 ;;; ================================================================
@@ -170,6 +164,29 @@ Specify to clear or create scratch buffer with ARG"
 ;;; programming
 ;;; ================================================================
 
+;; LSP
+(use-package lsp-mode
+  :ensure t
+  :init
+  (setq lsp-keymap-prefix "C-l")
+  (setq read-process-output-max (* 1024 1024))
+  (setq gc-cons-threshold 100000000)
+  (setq lsp-idle-delay 1.000)
+  (setq lsp-enable-file-watchers nil)
+  (setq lsp-disabled-clients '(ruby-ls ruby-lsp-ls ruby-syntax-tree-ls rubocop-ls typeprof-ls))
+  :hook
+  (ruby-mode . lsp)
+  (typescript-ts-mode . lsp)
+  ;; (tsx-ts-mode-hook . lsp)
+  (go-mode . lsp)
+  )
+;; don't work above settings for some reason
+(add-hook 'tsx-ts-mode-hook 'lsp)
+
+(use-package lsp-ui :ensure t)
+(setq lsp-sorbet-as-add-on t)
+(setq lsp-sorbet-use-bundler t)
+
 ;; format
 (defun my/use-prettier-from-node-modules ()
   "Use node_modules/.bin/prettier if exists."
@@ -186,8 +203,57 @@ Specify to clear or create scratch buffer with ARG"
   :hook
   (web-mode . prettier-js-mode)
   (typescript-ts-mode . prettier-js-mode)
+  ;; (tsx-ts-mode-hook . prettier-js-mode)
   (prettier-js-mode . my/use-prettier-from-node-modules))
+;; don't work above settings for some reason
+(add-hook 'tsx-ts-mode-hook 'prettier-js-mode)
 
+;; lint
+(use-package flycheck
+  :ensure t
+  :config
+  (setq-default flycheck-disabled-checkers
+                '(
+                  ember-template
+                  handlebars
+                  chef-foodcritic
+                  ruby-standard
+                  ruby-reek
+                  ruby-rubylint
+                  ruby-chef-cookstyle
+                  ruby-jruby
+                  ruby-rubocop
+                  typescript-tslint
+                  )
+                )
+
+  (flycheck-define-checker bundle/ruby-rubocop
+    "A Ruby syntax and style checker using the RuboCop tool.
+   Puts .dir-locals.el to each project root with contents
+
+     ((ruby-mode . ((flycheck-checker . bundle/ruby-rubocop))))
+
+  "
+    :command
+    ("bundle" "ex" "rubocop" "--format" "emacs"
+     (config-file "--config" flycheck-rubocoprc)
+     source)
+    :error-patterns
+    ((warning line-start
+              (file-name) ":" line ":" column ": " (or "C" "W") ": " (message)
+              line-end)
+     (error line-start
+            (file-name) ":" line ":" column ": " (or "E" "F") ": " (message)
+            line-end))
+    :modes (ruby-mode)
+    )
+  (add-to-list 'flycheck-checkers 'bundle/ruby-rubocop t)
+
+  :hook
+  (emacs-lisp-mode . flycheck-mode)
+  )
+
+;; misc
 (use-package company :ensure t)
 (use-package yafolding :ensure t
   :hook
--- inits/30-overwrite-helm.el
+++
@@ -1,4 +0,0 @@
-(add-hook 'helm-after-initialize-hook
-  (lambda ()
-    (defun helm-buffers-sort-transformer (candidates _source)
-      candidates)))
